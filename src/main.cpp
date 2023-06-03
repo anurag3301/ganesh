d5e90e3b3ebcc646f367c9dbcb3ce924c3f20f86
Anurag Kumar Singh <anurag3301.0x0@gmail.com>
2023-06-03 18:01:03 +0530

    Added simple webserver in nodemcu

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,81 @@
+
+#include <Arduino.h>
+
 #include <ESP8266WiFi.h>
+#include "secrets.h"
+/*
+    The "secrets.h" header contains secrets in following format
+    const char* ssid     = "ssid";
+    const char* password = "password";
+*/
+
+WiFiServer server(80);
+String header;
+
+unsigned long currentTime = millis();
+unsigned long previousTime = 0; 
+const long timeoutTime = 2000;
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("");
 
-  WiFi.mode(WIFI_STA);
-  WiFi.disconnect();
-  delay(100);
-
-  Serial.println("WiFi Netwoek Scan Started");
-}
-
-void loop() {
- 
-  int n = WiFi.scanNetworks();
-   
-  Serial.println("Scan done");
-  
-  if (n == 0)
-    Serial.println("No Networks Found");
-  else
-  {
-    Serial.print(n);
-    Serial.println(" Networks found");
-    for (int i = 0; i < n; ++i)
-    {
-      Serial.print(i + 1);
-      Serial.print(": ");
-      Serial.print(WiFi.SSID(i));
-      Serial.print(" (");
-      Serial.print(WiFi.RSSI(i));
-      Serial.print(") MAC:");
-      Serial.print(WiFi.BSSIDstr(i));
-      Serial.println((WiFi.encryptionType(i) == ENC_TYPE_NONE)?" Unsecured":" Secured");
-      delay(10);
-    }
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
   }
   Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
+}
 
-  delay(5000);
+void loop(){
+  WiFiClient client = server.available();
+
+  if (client) {
+    Serial.println("New Client.");
+    String currentLine = "";
+    currentTime = millis();
+    previousTime = currentTime;
+    while (client.connected() && currentTime - previousTime <= timeoutTime) {
+      currentTime = millis();         
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        header += c;
+        if (c == '\n') {
+          if (currentLine.length() == 0) {
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+            
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
+            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}</style></head>");
+            client.println("<body><h1>ESP8266 Web Server</h1>");
+            client.println("</body></html>");
+            
+            client.println();
+            break;
+          } else {
+            currentLine = "";
+          }
+        } else if (c != '\r') {
+          currentLine += c;
+        }
+      }
+    }
+    header = "";
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
 }
