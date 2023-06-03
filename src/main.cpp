5cc38d20245132f0e45c6f2c60b12d468dd3e5bf
Anurag Kumar Singh <anurag3301.0x0@gmail.com>
2023-06-03 18:19:53 +0530

    Reindented everything and removed htm

--- src/main.cpp
+++ src/main.cpp
@@ -34,48 +34,37 @@ void setup() {
 }
 
 void loop(){
-  WiFiClient client = server.available();
-
-  if (client) {
-    Serial.println("New Client.");
-    String currentLine = "";
-    currentTime = millis();
-    previousTime = currentTime;
-    while (client.connected() && currentTime - previousTime <= timeoutTime) {
-      currentTime = millis();         
-      if (client.available()) {
-        char c = client.read();
-        Serial.write(c);
-        header += c;
-        if (c == '\n') {
-          if (currentLine.length() == 0) {
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");
-            client.println();
-            
-            client.println("<!DOCTYPE html><html>");
-            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-            client.println("<link rel=\"icon\" href=\"data:,\">");
-            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-            client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
-            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}</style></head>");
-            client.println("<body><h1>ESP8266 Web Server</h1>");
-            client.println("</body></html>");
-            
-            client.println();
-            break;
-          } else {
-            currentLine = "";
-          }
-        } else if (c != '\r') {
-          currentLine += c;
+    WiFiClient client = server.available();
+    if (client) {
+        Serial.println("New Client.");
+        String currentLine = "";
+        currentTime = millis();
+        previousTime = currentTime;
+        while (client.connected() && currentTime - previousTime <= timeoutTime) {
+            currentTime = millis();         
+            if (client.available()) {
+                char c = client.read();
+                Serial.write(c);
+                header += c;
+                if (c == '\n') {
+                    if (currentLine.length() == 0) {
+                      client.println("HTTP/1.1 200 OK");
+                      client.println("Content-type:text/html");
+                      client.println("Connection: close");
+                      client.println();
+                      client.println();
+                      break;
+                    } else {
+                      currentLine = "";
+                    }
+                } else if (c != '\r') {
+                  currentLine += c;
+                }
+            }
         }
-      }
+        header = "";
+        client.stop();
+        Serial.println("Client disconnected.");
+        Serial.println("");
     }
-    header = "";
-    client.stop();
-    Serial.println("Client disconnected.");
-    Serial.println("");
-  }
 }
