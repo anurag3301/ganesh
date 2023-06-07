3253e0bb2dd75dbd6f7794d1b0f9a70b367f550a
Anurag Kumar Singh <anurag3301.0x0@gmail.com>
2023-06-07 16:11:02 +0530

    Added nodemcu stuff in backup and initialised the arduino mega

--- src/main.cpp
+++ src/main.cpp
@@ -1,74 +1,9 @@
-
 #include <Arduino.h>
 
-#include <ESP8266WiFi.h>
-#include "html.h"
-#include "secrets.h"
-/*
-    The "secrets.h" header contains secrets in following format
-    const char* ssid     = "ssid";
-    const char* password = "password";
-*/
+void setup(){
 
-WiFiServer server(80);
-String header;
-
-unsigned long currentTime = millis();
-unsigned long previousTime = 0; 
-const long timeoutTime = 2000;
-
-void setup() {
-  Serial.begin(115200);
-
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected.");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-  server.begin();
 }
 
 void loop(){
-    WiFiClient client = server.available();
-    if (client) {
-        Serial.println("New Client.");
-        String currentLine = "";
-        currentTime = millis();
-        previousTime = currentTime;
-        while (client.connected() && currentTime - previousTime <= timeoutTime) {
-            currentTime = millis();         
-            if (client.available()) {
-                char c = client.read();
-                Serial.write(c);
-                header += c;
-                if (c == '\n') {
-                    if (currentLine.length() == 0) {
-                      client.println("HTTP/1.1 200 OK");
-                      client.println("Content-type:text/html");
-                      client.println("Connection: close");
-                      client.println();
-                
-                      client.println(html);
-                      
-                      client.println();
-                      break;
-                    } else {
-                      currentLine = "";
-                    }
-                } else if (c != '\r') {
-                  currentLine += c;
-                }
-            }
-        }
-        header = "";
-        client.stop();
-        Serial.println("Client disconnected.");
-        Serial.println("");
-    }
+
 }
