df3902008b098d42c6d10fe461e84ed5a8ad6a12
Anurag Kumar Singh <anurag3301.0x0@gmail.com>
2023-06-07 17:16:48 +0530

    Added basic rainbom colourpallet

--- src/main.cpp
+++ src/main.cpp
@@ -2,30 +2,41 @@
 
 #define LED_PIN 5
 #define NUM_LEDS 10
+#define BRIGHTNESS 30
+#define UPDATES_PER_SECOND 100
 
 CRGB leds[NUM_LEDS];
 
-void setup() {
-    FastLED.addLeds < WS2812, LED_PIN, GRB > (leds, NUM_LEDS);
-    FastLED.setMaxPowerInVoltsAndMilliamps(5, 500);
-    FastLED.clear();
-    FastLED.show();
+CRGBPalette16 currentPalette;
+TBlendType currentBlending;
 
+void FillLEDsFromPaletteColors( uint8_t colorIndex);
+
+void setup() {
+    delay(1000);
+    Serial.begin(9600);
+    FastLED.addLeds < WS2812, LED_PIN, GRB > (leds, NUM_LEDS);
+    FastLED.setBrightness(BRIGHTNESS);
+    currentPalette = RainbowColors_p;
+    currentBlending = LINEARBLEND;
 }
 
 void loop() {
-    // RED Green Blue
-    for (int i = 0; i < NUM_LEDS; i++) {
-        leds[i] = CRGB(0, 255 - 2 * i, 20 * i);
-        FastLED.setBrightness(10);
-        FastLED.show();
-        delay(50);
-    }
-    for (int i = NUM_LEDS; i > 0; i--) {
-        leds[i] = CRGB(20 * i, 0, 255 - 20 * i);
-        FastLED.setBrightness(10);
-        FastLED.show();
-        delay(50);
 
+    static uint8_t startIndex = 0;
+    startIndex++;
+
+    FillLEDsFromPaletteColors( startIndex);
+    
+    FastLED.show();
+    FastLED.delay(1000 / UPDATES_PER_SECOND);
+}
+
+void FillLEDsFromPaletteColors( uint8_t colorIndex){
+    uint8_t brightness = 255;
+    
+    for( int i = 0; i < NUM_LEDS; i++) {
+        leds[i] = ColorFromPalette( currentPalette, colorIndex, brightness, currentBlending);
+        colorIndex += 3;
     }
 }
